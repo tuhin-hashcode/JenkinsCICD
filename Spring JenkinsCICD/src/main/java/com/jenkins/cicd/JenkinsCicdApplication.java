dd4fd6b36cd0995c04a2834e21eece723a73ec31
tuhinhussain1995 <145957894+tuhin-hashcode@users.noreply.github.com>
2024-01-27 14:31:42 +0800

    added initial project

---
+++ Spring JenkinsCICD/src/main/java/com/jenkins/cicd/JenkinsCicdApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsCicdApplication.class, args);
+	}
+
+}
