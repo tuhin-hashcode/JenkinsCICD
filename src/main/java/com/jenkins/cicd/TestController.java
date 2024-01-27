d032f0af32950681e391428870a70a057b98d152
Md Tuhin Hossain <145957894+tuhin-hashcode@users.noreply.github.com>
2024-01-27 19:03:36 +0800

    private

--- src/main/java/com/jenkins/cicd/TestController.java
+++ src/main/java/com/jenkins/cicd/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 	@GetMapping("/test")
 	public String test()
 	{
-		return "This is a test message just to make sure if Jenkins CICD working fine";
+		return "Test Successfull !!! Private";
 	}
 }
